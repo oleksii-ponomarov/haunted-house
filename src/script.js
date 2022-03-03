f13abcadadea1b6f96ae9ab047895dadcc8dc62d
Oleksii Ponomarov <rocknrollerr@gmail.com>
2022-03-03 20:43:30 +0200

    Some polish

--- src/script.js
+++ src/script.js
@@ -71,7 +71,10 @@ scene.add(camera);
 // Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
+controls.minDistance = 5;
 controls.maxDistance = 20;
+controls.maxPolarAngle = Math.PI * 0.5 - 0.05;
+controls.enablePan = false;
 
 /**
  * Renderer
